faf46ef426306d0ceca054206e2edb08b33b3065
Ashley-L <ashley.lall@outlook.com>
2020-03-29 20:13:13 -0400

    Added scroll-to on scrollspy.

--- js/main.js
+++ js/main.js
@@ -1,32 +1,33 @@
 // create character database
 // add characters in an array in order to populate each section
 
+let $topbarHeight = document.querySelector('.top-bar').getBoundingClientRect().height 
+
 
 // PART A
 
 // Scroll-to from character cards at top
 let $charCardLink = document.querySelector('.char-card-name')
-let $scrollToChar = document.querySelectorAll('.char-card-name')
-let $topbarHeight = document.querySelector('.top-bar').getBoundingClientRect().height 
+let $charCard = document.querySelectorAll('.char-card-name')
 
-$scrollToChar.forEach($charCardLink => {
+$charCard.forEach($charCardLink => {
     $charCardLink.addEventListener('click', event => {
         event.preventDefault();
 
         console.log(`clicked`);
 
         // Which link are you clicking <a href>
-        let $clickedLink = $charCardLink.getAttribute('href')
-        console.log(`you clicked ${$clickedLink}`)
+        let $clickedCharCard = $charCardLink.getAttribute('href')
+        console.log(`you clicked ${$clickedCharCard}`)
 
-        // variable to select the character <h3> you clicked
-        let $clickedChar = document.querySelector($clickedLink)
+        // variable to select the character <section> you clicked
+        var $clickedChar = document.querySelector($clickedCharCard)
         console.log($clickedChar)
 
         // get the scroll position of the top of the character section
         // subtract the topbar height so it slides to the right position (otherwise it's under the topbar)
         let $charSectionPosition = ($clickedChar.getBoundingClientRect().top - $topbarHeight) + window.scrollY 
-        console.log(`Scene top scroll position is ${$charSectionPosition}px`)
+        console.log(`section top scroll position is ${$charSectionPosition}px`)
 
         // scroll to the position of the character section 
         window.scrollTo({
@@ -39,4 +40,38 @@ $scrollToChar.forEach($charCardLink => {
 })
 
 
+// Match scrollspy to position in page
+
+let $scrollspyCharLink = document.querySelector('.scrollspy-char')
+console.log($scrollspyCharLink)
+let $scrollspyChar = document.querySelectorAll('.scrollspy-char')
+
+$scrollspyChar.forEach($scrollspyCharLink => {
+    $scrollspyCharLink.addEventListener('click', event => {
+        event.preventDefault()
+    
+        console.log(`clicked`)
+
+        let $clickedScrollspy = $scrollspyCharLink.getAttribute('href')
+        console.log($clickedScrollspy)
+
+        // variable to select the character <section> you clicked
+        var $clickedChar = document.querySelector($clickedScrollspy)
+        console.log($clickedChar)
+
+        let $charSectionPosition = ($clickedChar.getBoundingClientRect().top - $topbarHeight) + window.scrollY 
+        console.log(`section top scroll position is ${$charSectionPosition}px`)
+
+        // scroll to the position of the character section 
+        window.scrollTo({
+        left:0, 
+        top: $charSectionPosition, 
+        behavior:'smooth'
+        })
+
+
+
+
+    })
+})
 
